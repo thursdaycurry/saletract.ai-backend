aae9d3c419e143f4d86b67143b4b3f0676aa9958
thursdaycurry <thursdaycurry@gmail.com>
2023-12-06 22:52:12 +0900

    FEAT: add userLogin api

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/User.js';
-import { hash } from 'bcrypt';
+import { hash, compare } from 'bcrypt';
 
 export const getAllUsers = async (
   req: Request,
@@ -24,10 +24,35 @@ export const userSignup = async (
 ) => {
   try {
     const { name, email, password } = req.body;
-    const hashedPassword = await hash(password, 10);
+    const isExistingUser = await User.findOne({ email });
+    if (isExistingUser) {
+      return res.status(401).send('already registered user');
+    }
 
+    const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+    return res.status(200).json({ message: 'OK', id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: 'ERROR', cause: error.message });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).send('Cannot find the user');
+
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) return res.status(403).send('Wrong password');
 
     return res.status(200).json({ message: 'OK', id: user._id.toString() });
   } catch (error) {
